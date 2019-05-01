43154e843d5e723a199673537072cab6b1ab56b9
Dave Westerveld <Dave.Westerveld@D2L.com>
2019-05-01 16:03:58 -0400

    test cleanup

--- test/TestCafe/page-model.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-
-export default class loginPage {
-    constructor () {
-        this.userName =  Selector('#userName');
-        this.pwdField =  Selector('#password');
-        this.loginButton = Selector('button');
-    }
-}

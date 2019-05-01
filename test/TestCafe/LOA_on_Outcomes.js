f38a388cdf8aec3e60d1a8f8491557fe17e237cd
Dave Westerveld <Dave.Westerveld@D2L.com>
2019-05-01 16:09:09 -0400

    linter fixes

--- test/TestCafe/LOA_on_Outcomes.js
+++ test/TestCafe/LOA_on_Outcomes.js
@@ -7,14 +7,14 @@ test ('Add Button Available After Selecting First Outcome', async t => {
 		.navigateTo('http://127.0.0.1:8081/components/d2l-activity-alignments/demo/')
 		.wait(3000);//because webcomponts don't do 'end events' correctly so we have to make sure it is loaded before we go looking for stuff in the shadowRoots
 	const checkBox = Selector(() => {
-		const alignment = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot
+		const alignment = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot;
 		return alignment.querySelector('d2l-input-checkbox').shadowRoot.querySelector('label > input[type="checkbox"]');
 	});
 	const addButton = await Selector(() => {
-		const alignments = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot
-		return alignments.querySelector('d2l-button')
+		const alignments = document.querySelector('d2l-select-outcomes').shadowRoot.querySelector('d2l-alignment-update').shadowRoot;
+		return alignments.querySelector('d2l-button');
 	});
 	await t
 		.click(checkBox)
-		.expect(addButton.hasAttribute('disabled')).notOk()
+		.expect(addButton.hasAttribute('disabled')).notOk();
 });

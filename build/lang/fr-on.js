804c5aad5ff61df33c11339c2f80312d1359c8be
Devon Powell <Devon.Powell@D2L.com>
2021-03-30 11:49:52 -0400

    US125911 - Add fr-ON

---
+++ build/lang/fr-on.js
@@ -0,0 +1,42 @@
+import '@polymer/polymer/polymer-legacy.js';
+window.D2L = window.D2L || {};
+window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
+window.D2L.PolymerBehaviors.SelectOutcomes = window.D2L.PolymerBehaviors.SelectOutcomes || {};
+window.D2L.PolymerBehaviors.SelectOutcomes.LocalizeBehavior = window.D2L.PolymerBehaviors.SelectOutcomes.LocalizeBehavior || {};
+window.D2L.SelectOutcomes = window.D2L.SelectOutcomes || {};
+window.D2L.SelectOutcomes.Language = window.D2L.SelectOutcomes.Language || {};
+window.D2L.SelectOutcomes.Language.FrOn = {
+	'a11yHeaderAriaLabel': 'Niveau de l’arborescence {level} – {status} – {name}',
+	'a11yLeafAriaLabel': 'Élément de l’arbre {shortCode} – {status} – {description}',
+	'add': 'Ajouter',
+	'addLabel': 'Ajouter la sélection',
+	'alignmentRemoved': 'Alignement supprimé',
+	'cancel': 'Annuler',
+	'cancelLabel': 'Annuler la sélection',
+	'collapsed': 'réduit(e)',
+	'defaultTagType': 'Balises',
+	'directAlignments': '{headerTitle} aligné directement avec cette activité',
+	'error': 'Une erreur est survenue',
+	'expanded': 'développé(e)',
+	'indirectAlignments': '{headerTitle} aligné avec les critères de la grille d’évaluation',
+	'navigationDescription': '{title} : utilisez les flèches pour naviguer dans {typeName}, utilisez les touches Supprimer et Retour arrière pour supprimer {typeName}.',
+	'navigationDescriptionNoActions': '{title} : utilisez les flèches pour naviguer dans {typeName}.',
+	'noSearchResultFor': 'Aucun résultat trouvé pour "{searchText}".',
+	'notSelected': 'non sélectionné(e)',
+	'outcomesHierarchicalTree': 'Résultats de l’arborescence hiérarchique',
+	'removeAlignment': 'Supprimer l’alignement',
+	'searchCleared': 'Recherche effacée',
+	'searchOutcomes': 'Résultats de la recherche',
+	'searchPlaceholder': 'Recherche…',
+	'searchResultFor': '{numOfResults} résultat(s) de la recherche pour "{searchText}"',
+	'searchResultsNumber': '{numOfResults} résultat(s) de la recherche',
+	'selected': 'sélectionné(s)'
+};
+
+/*
+ * FrOn lang terms
+ * @polymerBehavior D2L.PolymerBehaviors.SelectOutcomes.LocalizeBehavior.LangFrOnBehavior
+ */
+D2L.PolymerBehaviors.SelectOutcomes.LocalizeBehavior.LangFrOnBehavior = {
+	frOn: window.D2L.SelectOutcomes.Language.FrOn
+};

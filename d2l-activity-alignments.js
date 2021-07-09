5e61064ceef352a34703d0d8dfcd8eabbb98f7e6
Callum <mitccall@gmail.com>
2021-07-09 14:59:23 -0400

    Draft: convert d2l-activity-alignments

--- d2l-activity-alignments.js
+++ d2l-activity-alignments.js
@@ -1,27 +1,45 @@
 /**
-`d2l-select-outcomes`
+`d2l-activity-alignments`
 
 @demo demo/index.html
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
-
-import 'd2l-polymer-siren-behaviors/store/entity-behavior.js';
 import { Rels } from 'd2l-hypermedia-constants';
 import 'd2l-alert/d2l-alert.js';
 import './d2l-alignment-list.js';
 import './d2l-user-alignment-list.js';
 import './localize-behavior.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-const $_documentContainer = document.createElement('template');
+import { LocalizeMixin } from './LocalizeMixin';
+import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit';
+import { css, html } from 'lit-element';
 
-$_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-activity-alignments">
-	<template strip-whitespace="">
-		<style>
+class D2lActivityAlignments extends LocalizeMixin(EntityMixinLit(LitElement)) {
+
+	static get is() { return 'd2l-activity-alignments'; }
+
+	static get properties() {
+		return {
+			headerTitle: {
+				type: String,
+				attribute: 'header-title'
+			},
+			readOnly: {
+				type: Boolean,
+				attribute: 'read-only'
+			},
+			_alignmentsHref: {
+				type: String
+			},
+			_isUserOrActorActivityUsage: {
+				type: Boolean
+			},
+			_showError: {
+				type: Boolean
+			}
+		}
+	}
+
+	static get styles() {
+		return css`
 			:host {
 				display: flex;
 			}
@@ -33,81 +51,98 @@ $_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-activity-alignments
 				height: 100%;
 				z-index: 0;
 			}
-		</style>
-		<div class="d2l-activity-alignments-main">
-			<template is="dom-if" if="[[_isUserOrActorActivityUsage(entity)]]">
-				<d2l-user-alignment-list href="[[_getAlignments(entity)]]" token="[[token]]" read-only$="[[readOnly]]">
-					<slot name="outcomes-title" slot="outcomes-title"></slot>
-					<slot name="show-select-outcomes" slot="show-select-outcomes"></slot>
-					<slot name="describe-aligned-outcomes" slot="describe-aligned-outcomes"></slot>
-				</d2l-alignment-list>
-			</template>
-			<template is="dom-if" if="[[!_isUserOrActorActivityUsage(entity)]]">
-				<d2l-alignment-list href="[[_getAlignments(entity)]]" token="[[token]]" read-only$="[[readOnly]]" header-title="[[headerTitle]]">
-					<slot name="outcomes-title" slot="outcomes-title"></slot>
-					<slot name="show-select-outcomes" slot="show-select-outcomes"></slot>
-					<slot name="describe-aligned-outcomes" slot="describe-aligned-outcomes"></slot>
-				</d2l-alignment-list>
-			</template>
-			<template is="dom-if" if="[[_showError]]">
-				<d2l-alert type="error">[[localize('error')]]</d2l-alert>
-			</template>
-		</div>
-	</template>
-
-
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
-Polymer({
-
-	is: 'd2l-activity-alignments',
-
-	properties: {
-		readOnly: Boolean,
-
-		_showError: {
-			type: Boolean,
-			value: false
-		},
-		headerTitle: {
-			type: String,
-			value: null
-		}
-	},
-
-	behaviors: [
-		D2L.PolymerBehaviors.Siren.EntityBehavior,
-		window.D2L.PolymerBehaviors.SelectOutcomes.LocalizeBehavior,
-	],
-
-	ready: function() {
-		this._boundHandleError = this._handleError.bind(this);
-	},
-
-	attached: function() {
-		this._showError = false;
-		this.addEventListener('d2l-siren-entity-error', this._boundHandleError);
-	},
-
-	detached: function() {
-		this.removeEventListener('d2l-siren-entity-error', this._boundHandleError);
-	},
-
-	_handleError: function() {
-		this._showError = true;
-	},
-
-	_getAlignments: function(entity) {
-		return entity && entity.hasLinkByRel(Rels.Alignments.alignments) && entity.getLinkByRel(Rels.Alignments.alignments).href;
-	},
-
-	_isUserOrActorActivityUsage: function(entity) {
-		if (!entity) return undefined;
-		const selfLink = entity.getLinkByRel('self');
-		if (!selfLink) return undefined;
-		return selfLink.rel.some(rel => (rel === Rels.Activities.userActivityUsage ||
-			rel === Rels.Activities.actorActivityUsage));
+		`;
 	}
 
-});
+	constructor() {
+		super();
+		
+		this.headerTitle = '';
+		this.readOnly = false;
+		this._alignmentsHref = '';
+		this._isUserOrActorActivityUsage = false;
+		this._showError = false;
+
+		//this._setEntityType() //Need to make entity object first
+
+		this._boundHandleError = this._handleError.bind(this);
+	}
+
+	connectedCallback() {
+		this._showError = false;
+		this.addEventListener('d2l-siren-entity-error', this._boundHandleError);
+	}
+
+	disconnectedCallback() {
+		this.removeEventListener('d2l-siren-entity-error', this._boundHandleError);
+	}
+
+	render() {
+		return html`
+		<div class="d2l-activity-alignments-main">
+			${this._isUserOrActorActivityUsage ? this._renderUserAlignmentList() : this._renderAlignmentList() }
+			${this._showError ? this._renderErrorAlert() : html`` }
+		</div>
+		`;
+	}
+
+	set _entity(entity) {
+		if (this._entityHasChanged(entity)) {
+			this._onEntityChanged(entity);
+			super._entity = entity;
+		}
+	}
+
+	_handleError() {
+		this._showError = true;
+	}
+
+	//TODO: can probably move this to the entity object once defined for reusability
+	_getAlignmentsHref(entity) {
+		return entity && entity.hasLinkByRel(Rels.Alignments.alignments) && entity.getLinkByRel(Rels.Alignments.alignments).href;
+	}
+
+	_onEntityChanged(entity) {
+		if (!entity) {
+			return;
+		}
+
+		const selfLink = entity.getLinkByRel('self');
+		if (selfLink) {
+			this._isUserOrActorActivityUsage = selfLink.rel.some(rel => (
+				rel === Rels.Activities.userActivityUsage ||
+				rel === Rels.Activities.actorActivityUsage
+			));
+		}
+
+		this._alignmentsHref = this._getAlignmentsHref(entity);
+	}
+
+	_renderAlignmentList() {
+		return html`
+			<d2l-alignment-list href="${this._alignmentsHref}" token="${this.token}" read-only="${this.readOnly}" header-title="${this.headerTitle}">
+				<slot name="outcomes-title" slot="outcomes-title"></slot>
+				<slot name="show-select-outcomes" slot="show-select-outcomes"></slot>
+				<slot name="describe-aligned-outcomes" slot="describe-aligned-outcomes"></slot>
+			</d2l-alignment-list>
+		`;
+	}
+
+	_renderErrorAlert() {
+		return html`
+			<d2l-alert type="error">${this.localize('error')}</d2l-alert>
+		`;
+	}
+
+	_renderUserAlignmentList() {
+		return html`
+			<d2l-user-alignment-list href="${this._alignmentsHref}" token="${this.token}" read-only="${this.readOnly}">
+				<slot name="outcomes-title" slot="outcomes-title"></slot>
+				<slot name="show-select-outcomes" slot="show-select-outcomes"></slot>
+				<slot name="describe-aligned-outcomes" slot="describe-aligned-outcomes"></slot>
+			</d2l-user-alignment-list>
+		`;
+	}
+}
+
+customElements.define(D2lActivityAlignments.is, D2lActivityAlignments);

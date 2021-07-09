2f21d6493d8646fd2c1e61b69b3ad47e54dc4b69
Callum <mitccall@gmail.com>
2021-07-09 15:59:24 -0400

    Draft: convert d2l-activity-alignment-tags to lit

--- d2l-activity-alignment-tags.js
+++ d2l-activity-alignment-tags.js
@@ -1,89 +1,85 @@
-import { PolymerElement, html } from '@polymer/polymer';
-import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
-import 'd2l-polymer-siren-behaviors/store/entity-behavior.js';
-import 'd2l-hypermedia-constants/d2l-hypermedia-constants.js';
+import { LocalizeMixin } from './LocalizeMixin';
+import { EntityMixinLit } from 'siren-sdk/src/mixin/entity-mixin-lit';
+import { css, html } from 'lit-element';
+import { Rels } from 'd2l-hypermedia-constants';
 import 'd2l-alert/d2l-alert.js';
 import './d2l-activity-alignment-tag-list.js';
 import './localize-behavior.js';
 
-class ActivityAlignmentTags extends mixinBehaviors([
-	D2L.PolymerBehaviors.Siren.EntityBehavior,
-	D2L.Hypermedia.HMConstantsBehavior,
-	D2L.PolymerBehaviors.SelectOutcomes.LocalizeBehavior
-], PolymerElement) {
+class ActivityAlignmentTags extends LocalizeMixin(EntityMixinLit(LitElement)) {
 
 	static get is() {
 		return 'd2l-activity-alignment-tags';
 	}
 
+	static get styles() {
+		return css`
+			:host {
+				display: flex;
+				flex-direction: column;
+				align-items: stretch;
+			}
+		`;
+	}
 	static get properties() {
 		return {
-			readOnly: Boolean,
-			empty: {
+			readOnly: {
 				type: Boolean,
-				notify: true,
-				readOnly: true,
-				computed: '_isEmpty(entity, _tagListIsEmpty)'
+				attribute: 'read-only'
+			},
+			empty: {
+				type: Boolean
 			},
 			_showError: {
-				type: Boolean,
-				value: false
-			},
+				type: Boolean
+			},			
+			//TODO: create event to notify parent of value change
 			_tagListIsEmpty: {
 				type: Boolean,
-				value: true
 			},
 			browseOutcomesText: {
 				type: String,
-				value: null,
-				reflectToAttribute: true
+				attribute: 'browse-outcomes-text',
+				reflect: true
 			},
 			deferredSave: {
-				type: Boolean,
-				value: false
+				type: Boolean
 			},
 			hideIndirectAlignments: {
-				type: Boolean,
-				value: false
+				type: Boolean
 			},
 			typeName: {
 				type: String,
-				value: null,
-				reflectToAttribute: true
+				reflect: true
 			},
 			title: {
 				type: String,
-				value: null,
-				reflectToAttribute: true
+				reflect: true
 			}
 		};
 	}
 
-	static get template() {
+	constructor() {
+		super();
+
+		this.browseOutcomesText = null;
+		this.deferredSave = false;
+		this.empty = false;
+		this.hideIndirectAlignments = false;
+		this.title = null;
+		this.typeName = null;
+		this._showError = false;
+		this._tagListIsEmpty = true;
+
+		//this._setEntityType() //Need to make entity object first
+
+		this._boundHandleError = this._handleError.bind(this);
+	}
+
+	render() {
 		return html`
-			<style>
-				:host {
-					display: flex;
-					flex-direction: column;
-					align-items: stretch;
-				}
-			</style>
-			<template is="dom-if" if="[[_getAlignments(entity)]]" restamp>
-				<d2l-activity-alignment-tag-list
-					href="[[_getAlignments(entity)]]"
-					token="[[token]]"
-					read-only="[[readOnly]]"
-					empty="{{_tagListIsEmpty}}"
-					browse-outcomes-text="[[browseOutcomesText]]"
-					deferred-save="[[deferredSave]]"
-					hide-indirect-alignments="[[hideIndirectAlignments]]"
-					title="[[title]]"
-					type-name="[[typeName]]"
-				></d2l-activity-alignment-tag-list>
-			</template>
-			<template is="dom-if" if="[[_showError]]">
-				<d2l-alert type="error">[[localize('error')]]</d2l-alert>
-			</template>
+			${this._alignmentsHref ? this._renderTagList() : html`` }
+			${this._showError ? this._renderErrorAlert() : html``}
 		`;
 	}
 
@@ -99,25 +95,58 @@ class ActivityAlignmentTags extends mixinBehaviors([
 		super.disconnectedCallback();
 	}
 
-	ready() {
-		this._boundHandleError = this._handleError.bind(this);
-		super.ready();
+	set _entity(entity) {
+		if (this._entityHasChanged(entity)) {
+			this._onEntityChanged(entity);
+			super._entity = entity;
+		}
 	}
 
-	_getAlignments(entity) {
+	//TODO: can probably add to entity object once complete for reusability
+	_getAlignmentsHref(entity) {
 		return entity
-			&& entity.hasLinkByRel(this.HypermediaRels.Alignments.alignments)
-			&& entity.getLinkByRel(this.HypermediaRels.Alignments.alignments).href;
+			&& entity.hasLinkByRel(Rels.Alignments.alignments)
+			&& entity.getLinkByRel(Rels.Alignments.alignments).href;
 	}
 
 	_handleError() {
 		this._showError = true;
 	}
 
-	_isEmpty(entity, tagListIsEmpty) {
-		return !this._getAlignments(entity) || tagListIsEmpty;
+	_isEmpty(alignmentsHref, tagListIsEmpty) {
+		return !alignmentsHref || tagListIsEmpty;
 	}
 
+	_onEntityChanged(entity) {
+		if (!entity) {
+			return;
+		}
+
+		this._alignmentsHref = this._getAlignmentsHref(entity);
+		this.empty = this._isEmpty(this.alignmentsHref, this._tagListIsEmpty);
+	}
+
+	_renderErrorAlert() {
+		return html`
+			<d2l-alert type="error">${this.localize('error')}</d2l-alert>
+		`;
+	}
+
+	_renderTagList() {
+		return html`
+			<d2l-activity-alignment-tag-list
+				href="${this._alignmentsHref}"
+				token=${this.token}"
+				read-only="${this.readOnly}"
+				empty="${this._tagListIsEmpty /*TODO: create event to notify parent of value change*/}"
+				browse-outcomes-text="[[browseOutcomesText]]"
+				deferred-save="[[deferredSave]]"
+				hide-indirect-alignments="[[hideIndirectAlignments]]"
+				title="[[title]]"
+				type-name="[[typeName]]"
+			></d2l-activity-alignment-tag-list>
+		`;
+	}
 }
 
 customElements.define(ActivityAlignmentTags.is, ActivityAlignmentTags);
